ee59bb1ce6cb42208b7915315fef95c6bbd9789c
Marshall C <MCass2101@gmail.com>
2021-11-03 09:42:24 -0400

    book viewing logic made and split to own file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import TitleBar from './TitleBar/TitleBar';
+import BookViewer from './BookViewer/BookViewer';
 
 class App extends Component {
     constructor(props){
@@ -15,21 +16,33 @@ class App extends Component {
         };
     }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+        tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        })
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="row">
-                    <div className="col-md-4">
-                        {/* Button here to move to previous book*/}</div>
-                    <div className="col-md-4"> 
-                        {/* Display current book*/}
-                        <h1>{this.books[this.state.bookNumber].title}</h1>
-                        <h4>{this.books[this.state.bookNumber].author}</h4>
-                    </div>
-                    <div className="col-md-4">
-                        {/* Button here to move to next book*/}</div>
-                </div>
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
             </div>
         )
     }
